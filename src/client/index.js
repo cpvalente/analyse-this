419c531bfd2b82260b1fb1f6fa9c36f8218ea560
cv <34649812+cpvalente@users.noreply.github.com>
2021-06-07 22:20:39 +0200

    scss

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,6 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
-
-console.log(checkForName);
-
-alert("I EXIST")
-console.log("CHANGE!!");
+import { handleSubmit } from './js/formHandler';
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+import './styles/resets.scss';
